de182f4777ad9cfe20cce1989cb4441b9601a700
ActionCactus <n.atkocaitis@gmail.com>
2018-07-23 22:39:34 -0400

    Got an extremely crude movement system working

--- fps/Assets/Src/Controllers/StateMachines/PlayerCharacter.cs
+++ fps/Assets/Src/Controllers/StateMachines/PlayerCharacter.cs
@@ -6,7 +6,7 @@ namespace ActionCactus.Controllers.StateMachines
 {
     public class PlayerCharacter : MonoBehaviour
     {
-
+        private
         // Use this for initialization
         void Start()
         {
@@ -16,7 +16,25 @@ namespace ActionCactus.Controllers.StateMachines
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetKey("w"))
+            {
+                this.transform.Translate(0, 0, 1);
+            }
 
+            if (Input.GetKey("s"))
+            {
+                this.transform.Translate(0, 0, -1);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                this.transform.Translate(-1, 0, 0);
+            }
+
+            if (Input.GetKey("d"))
+            {
+                this.transform.Translate(1, 0, 1);
+            }
         }
     }
 }
